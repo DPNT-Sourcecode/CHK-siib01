dea6b245677a757a9c0bb1a881ee316dbcdf605a
sbx_user1051 <sbx_user1051@169.254.111.197>
2022-07-15 12:48:45 +0000

    Fri Jul 15 12:48:45 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,39 @@
 
 from dataclasses import dataclass
+from operator import attrgetter
+from typing import Callable, Sequence
+
+@dataclass
+class Item:
+    name: str
+    price: int
+
+@dataclass
+class SpecialOffer:
+    predicates: Sequence[Sequence[Callable]]
 
 
+ITEMS = {
+    "A": Item(name="A", price=50),
+    "B": Item(name="B", price=30),
+    "C": Item(name="C", price=20),
+    "D": Item(name="D", price=15),
+}
 
+SPECIAL_OFFERS = [
+    SpecialOffer(predicates=[[lambda item: item.name == "A"] * 3])
+]
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    try:
+        items = [ITEMS[sku] for sku in skus]
+    except KeyError:
+        return -1
+
+    return sum(map(attrgetter("price"), items))
+
+if __name__ == "__main__":
+    print(checkout("AAAA"))
 
