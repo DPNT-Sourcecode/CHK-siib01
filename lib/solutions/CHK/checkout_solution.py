e1f0f226c306493b6ef7872d68d63730cdbd4620
sbx_user1051 <sbx_user1051@169.254.111.197>
2022-07-15 13:39:05 +0000

    Fri Jul 15 13:39:05 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,38 +1,38 @@
 
-from dataclasses import dataclass
-from operator import attrgetter
-from typing import Callable, Sequence
-
-@dataclass
-class Item:
-    name: str
-    price: int
-
-@dataclass
-class SpecialOffer:
-    predicates: Sequence[Sequence[Callable]]
-
-
-ITEMS = {
-    "A": Item(name="A", price=50),
-    "B": Item(name="B", price=30),
-    "C": Item(name="C", price=20),
-    "D": Item(name="D", price=15),
-}
-
-SPECIAL_OFFERS = [
-    SpecialOffer(predicates=[[lambda item: item.name == "A"] * 3])
-]
-
+from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     try:
-        items = [ITEMS[sku] for sku in skus]
-    except KeyError:
-        return -1
+        # counter dictionary of all items 
+        items = Counter(skus)
+        prices = [50,30,20,15]
 
-    return sum(map(attrgetter("price"), items))
+        # check item validity
+        valid_items = ['A', 'B', 'C', 'D']
+        for item in items.keys():
+            if item not in valid_items:
+                return -1
+        
+        ## above could be combined into this if efficiency is required on huge lists + loops optimized
+        ## for a smaller list like this I have favoured readability of the code to maintain
+        # calculate cost of each item and total
+        total = 0 
+        for idx, item in enumerate(valid_items):
+            quantity = items[item]
+            price = prices[idx]
+            # instructions for A
+            if item == 'A':
+                cost = quantity%3*price + int(quantity/3)*130
+            # instructions for B
+            elif item == 'B':
+                cost = quantity%2*price + int(quantity/2)*45
+            # others
+            else:
+                cost = quantity*price
+            total += cost
 
-if __name__ == "__main__":
-    print(checkout("AAAA"))
+        return total
+
+    except:
+        raise NotImplementedError()
